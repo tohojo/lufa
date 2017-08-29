a5a57d172e9fd3cb9c41e77182f53d8c5653f98a
Toke Høiland-Jørgensen <toke.hoiland-jorgensen@kau.se>
2017-08-29 14:45:05 +0200

    Add KAU LDD project for temperature sensor and LED blinking device
    This adds a project that implements the KAU LDD temperature sensor and LED
    blinking device. The project is based on the BulkVendor demo, but changed to
    report temperature and support changing the LED state.
    
    The device presents itself with vendor ID 03eb and device ID 206c and contains
    to interrupt type endpoints for input and output. The input endpoint sends
    simple messages each consisting of two bytes. The first byte is a signed integer
    that represents the current temperature reading in degrees celcius. The second
    byte is an unsigned integer with a bitmask of the currently enabled LEDs (the
    lower four bits represent the four LEDs available on the device).
    
    The output endpoint will read single byte messages from the host which are
    simply applied as a new bitmap of enabled LEDs.
    
    The project also contains a Python script that will communicate with the device.
    This script has two operating modes: One that uses the pyusb library to talk
    directly to the device, and one that opens the device file (/dev/kauldd0)
    exported by the kernel mode driver. The script will run in libusb mode by
    default, and switch to kernel mode if passed 'kernel' as a command line
    parameter.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke.hoiland-jorgensen@kau.se>

---
+++ Projects/KauLdd/HostTestApp/temp-led-control.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+# Control app for the KAU LDD USB device; shows current temperature and allows
+# control of the LEDs
+
+import asyncio
+import sys
+import struct
+import tty
+import termios
+import usb.core
+import usb.util
+
+# Bulk Vendor HID device VID and PID
+device_vid = 0x03EB
+device_pid = 0x206C
+device_in_ep = 3
+device_out_ep = 4
+
+LEDS = [
+    1 << 0,
+    1 << 1,
+    1 << 2,
+    1 << 3]
+
+LED_CHARS = ['.', 'o']
+
+INTERVAL = 0.02
+
+
+class UsbDev(object):
+
+    def __init__(self, loop):
+        self.led_status = 0
+        self.loop = loop
+
+    def print_leds(self):
+        return "LEDs: " + "".join([LED_CHARS[min(1, self.led_status & m)]
+                                   for m in LEDS])
+
+    def toggle_led(self, no):
+        self.led_status ^= LEDS[no]
+        self.write(struct.pack("B", self.led_status))
+
+    def write(self, packet):
+        raise NotImplementedError()
+
+    def read(self):
+        raise NotImplementedError()
+
+    def show_status(self):
+        packet = self.read()
+        temp, self.led_status = struct.unpack("bB", packet)
+        print("\rCurrent temperature: %d°C - %s" % (temp, self.print_leds()),
+              end='')
+
+        self.loop.call_later(INTERVAL, self.show_status)
+
+
+class UsbDevLibUsb(UsbDev):
+
+    def __init__(self, loop):
+        super(UsbDevLibUsb, self).__init__(loop)
+        self.dev_handle = usb.core.find(idVendor=device_vid, idProduct=device_pid)
+
+        if self.dev_handle is None:
+            print("No valid Vendor device found.")
+            sys.exit(1)
+
+        print("Connected to device 0x%04X/0x%04X - %s [%s]" %
+              (self.dev_handle.idVendor, self.dev_handle.idProduct,
+               usb.util.get_string(self.dev_handle,
+                                   self.dev_handle.iProduct),
+               usb.util.get_string(self.dev_handle,
+                                   self.dev_handle.iManufacturer)))
+
+    def write(self, packet):
+        self.dev_handle.write(usb.util.ENDPOINT_OUT | device_out_ep, packet)
+
+    def read(self):
+        return self.dev_handle.read(usb.util.ENDPOINT_IN | device_in_ep, 64)
+
+class UsbDevKernel(UsbDev):
+
+    def __init__(self, loop):
+        super(UsbDevKernel, self).__init__(loop)
+        self.fp = open("/dev/kauldd0", "r+b", buffering=0)
+
+        print("Connected to /dev/kauldd0")
+
+    def write(self, packet):
+        self.fp.write(packet)
+
+    def read(self):
+        return self.fp.read(2)
+
+
+def read_char(dev):
+    c = sys.stdin.read(1)
+    if c in '1234':
+        dev.toggle_led(int(c)-1)
+
+    if c == 'q':
+        raise KeyboardInterrupt()
+
+
+def main(loop, mode):
+
+    if mode == 'kernel':
+        print("Using kernel mode")
+        dev = UsbDevKernel(loop)
+    else:
+        print("Using libusb mode")
+        dev = UsbDevLibUsb(loop)
+
+    loop.add_reader(sys.stdin.fileno(), read_char, dev)
+    loop.call_soon(dev.show_status)
+
+    print("Press q to exit, 1-4 to toggle LEDs", end='\n\n')
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.close()
+    return
+
+
+if __name__ == '__main__':
+    stdin_fd = sys.stdin.fileno()
+    term_settings = termios.tcgetattr(stdin_fd)
+
+    if len(sys.argv) < 2 or sys.argv[1] != 'kernel':
+        mode = 'libusb'
+    else:
+        mode = 'kernel'
+    try:
+        tty.setcbreak(stdin_fd)
+        loop = asyncio.get_event_loop()
+        main(loop, mode)
+    finally:
+        termios.tcsetattr(stdin_fd, termios.TCSADRAIN, term_settings)
+        print("")
