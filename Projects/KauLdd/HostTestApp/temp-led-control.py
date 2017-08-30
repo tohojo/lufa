4d1aeb5e26402b53745377d550a2a52734a3dfcf
Toke Høiland-Jørgensen <toke@toke.dk>
2017-08-30 11:49:33 +0200

    Turn temp-led-control into an interactive control script

--- Projects/KauLdd/HostTestApp/temp-led-control.py
+++ Projects/KauLdd/HostTestApp/temp-led-control.py
@@ -1,9 +1,11 @@
 # Control app for the KAU LDD USB device; shows current temperature and allows
 # control of the LEDs
 
+import asyncio
 import sys
 import struct
-import time
+import tty
+import termios
 import usb.core
 import usb.util
 
@@ -14,10 +16,23 @@ device_in_ep = 3
 device_out_ep = 4
 
 LEDS = [
-    1<<4,
-    1<<5,
-    1<<7,
-    1<<6]
+    1 << 4,
+    1 << 5,
+    1 << 7,
+    1 << 6]
+
+LED_CHARS = ['.', 'o']
+
+LED_STATUS = 0
+
+
+def print_leds(mask):
+    return "LEDs: " + "".join([LED_CHARS[min(1, mask & m)] for m in LEDS])
+
+
+def toggle_led(no):
+    return LED_STATUS ^ LEDS[no]
+
 
 def get_vendor_device_handle():
     dev_handle = usb.core.find(idVendor=device_vid, idProduct=device_pid)
@@ -26,28 +41,29 @@ def get_vendor_device_handle():
 
 def write(device, packet):
     device.write(usb.util.ENDPOINT_OUT | device_out_ep, packet)
-    print("Sent Packet: 0x{0}".format("".join(["%x" % x for x in packet])))
 
 
-def read(device):
+def show_status(device, loop=None):
+    global LED_STATUS
     packet = device.read(usb.util.ENDPOINT_IN | device_in_ep, 64)
-    print("Received Packet: 0x{0}".format(''.join(["%x" % x for x in packet])))
-    return struct.unpack("bB", packet)
+    temp, leds = struct.unpack("bB", packet)
+    LED_STATUS = leds
+    print("\rCurrent temperature: %d°C - %s" % (temp, print_leds(leds)), end='')
+
+    if loop:
+        loop.call_later(0.1, show_status, device, loop)
 
 
-def main():
-    if len(sys.argv) >= 2:
+def read_char(dev):
+    c = sys.stdin.read(1)
+    if c in '1234':
+        write(dev, struct.pack("B", toggle_led(int(c)-1)))
 
-        if sys.argv[1].startswith("0x"):
-            mask = int(sys.argv[1][2:], 16)
-        else:
-            mask = int(sys.argv[1])
-        if mask > 255 or mask < 0:
-            sys.stderr.write("LED_MASK must be > 0 and <= 255\n")
-            sys.exit(1)
-    else:
-        mask = None
+    if c == 'q':
+        raise KeyboardInterrupt()
 
+
+def main(loop):
 
     vendor_device = get_vendor_device_handle()
 
@@ -62,13 +78,27 @@ def main():
            usb.util.get_string(vendor_device, vendor_device.iProduct),
            usb.util.get_string(vendor_device, vendor_device.iManufacturer)))
 
-    if mask:
-        write(vendor_device, struct.pack("B", mask))
-    while True:
-        data = read(vendor_device)
-        print("Current temperature: %d°C" % struct.unpack("bB", data)[0])
-        time.sleep(1)
+    loop.add_reader(sys.stdin.fileno(), read_char, vendor_device)
+    loop.call_soon(show_status, vendor_device, loop)
+
+    print("Press q to exit, 1-4 to toggle LEDs", end='\n\n')
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.close()
+    return
 
 
 if __name__ == '__main__':
-    main()
+    stdin_fd = sys.stdin.fileno()
+    term_settings = termios.tcgetattr(stdin_fd)
+    try:
+        tty.setcbreak(stdin_fd)
+        loop = asyncio.get_event_loop()
+        main(loop)
+    finally:
+        termios.tcsetattr(stdin_fd, termios.TCSADRAIN, term_settings)
+        print("")
